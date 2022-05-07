01093a17902654567a1fdcf1ba0d2ffa779f021e
NitheshHS <66508499+NitheshHS@users.noreply.github.com>
2022-05-07 11:41:00 +0530

    Update CucumberRunner.java

--- src/test/java/runner/CucumberRunner.java
+++ src/test/java/runner/CucumberRunner.java
@@ -3,7 +3,7 @@ package runner;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(features = {"/src/test/java/feature/Login.feature"},//path of feature file
+@CucumberOptions(features = {"./src/test/java/feature/Login.feature"},//path of feature file
 glue = {"stepDefinitions"},//package name
 dryRun = false,// when it is true it will map both feature and step definition for missing steps
 //tags = "@fast",
