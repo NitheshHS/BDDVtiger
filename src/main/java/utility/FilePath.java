afef214706023b0a163e535acbd7a6653744a8de
NitheshHS <66508499+NitheshHS@users.noreply.github.com>
2022-05-07 11:43:12 +0530

    Update FilePath.java

--- src/main/java/utility/FilePath.java
+++ src/main/java/utility/FilePath.java
@@ -2,7 +2,7 @@ package utility;
 
 public interface FilePath {
 	
-	String PROPERTY_FILE=".\\src\\test\\resource\\CommonData.properties";
-	String SCREENSHOT_FOLDER=".\\screenshot\\";
+	String PROPERTY_FILE="./src/test/resource/CommonData.properties";
+	String SCREENSHOT_FOLDER="./screenshot/";
 
 }
